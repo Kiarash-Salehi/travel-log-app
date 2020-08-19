e445117f5024451b8bdf79b33794266bb5e5cc4d
KSDragon82 <www.kiarashs2929@gmail.com>
2020-08-19 20:09:26 +0430

    Forth time trying to deploy to heroku

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(express.json());
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
