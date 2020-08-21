0fe509cb4f6a0eed0afc303ebecf3463ca299439
KSDragon82 <www.kiarashs2929@gmail.com>
2020-08-21 20:33:35 +0430

    eighth time trying to deploy to heroku

---
+++ client/build/precache-manifest.17f33d30664da287f9592e358174da83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce1c7c17e1d636012b83394f5e413e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a396bf4d221d519c48c",
+    "url": "/static/css/main.38162da9.chunk.css"
+  },
+  {
+    "revision": "2360fe2d7cde6a7c74ce",
+    "url": "/static/js/2.976387a6.chunk.js"
+  },
+  {
+    "revision": "61eedd39499d34d7aaf09803c460ef46",
+    "url": "/static/js/2.976387a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a396bf4d221d519c48c",
+    "url": "/static/js/main.9335e6a4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
