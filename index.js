1840e3ce7010ffd46a5aeb7e45e7ddd2da4cda09
KSDragon82 <www.kiarashs2929@gmail.com>
2020-08-22 15:37:10 +0430

    11

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ const logsRouter = require('./api/logs');
 
 app.use(morgan('common'));
 app.use(helmet());
-app.use(cors({
-  origin: 'https://travel-log-mern-stack.herokuapp.com',
-}));
+app.use(cors());
 app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+if (process.env.NODE_ENV !== 'production') {
+  app.use(express.static(__dirname + '/client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.use('/', logsRouter);
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, () => {
+  console.log('connected');
+});
